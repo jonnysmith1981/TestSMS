e8988b304fd4585b7dae10a3101e59d248742aa4
jonnymith1981 <jonathan.smith@mis-ams.com>
2016-10-19 11:30:00 +0100

    Initial commit - Jonathan Smith

---
+++ app/src/main/java/com/example/jonathans/testsms/SMSCore.java
@@ -0,0 +1,44 @@
+package com.example.jonathans.testsms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by jonathans on 19/10/2016.
+ */
+
+public class SMSCore extends BroadcastReceiver {
+    private static final String SMS_RECEIVED = "android.provider.Telephone.SMS_RECEVIED";
+    private static final String TAG = "SMSBroadcastReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Intent received: " + intent.getAction());
+
+        if (intent.getAction().equals(SMS_RECEIVED)) {
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                Object[] pdus = (Object[])bundle.get("pdus");
+                final SmsMessage[] messages = new SmsMessage[pdus.length];
+                for (int i = 0; i < pdus.length; i++) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        String format = bundle.getString("format");
+                        messages[i] = SmsMessage.createFromPdu((byte[])pdus[i], format);
+                    }
+                    else {
+                        messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                    }
+                }
+                if (messages.length > -1) {
+                    Toast.makeText(context, "Message Received: " + messages[0].getMessageBody(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+}
