16ea557c06b52c12417622b235835f9acff3f0b8
Jonathan <jonathan@alpine-software.co.uk>
2016-10-19 19:01:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# TestSMS
+A proof-of-concept Android application to detect incoming SMS messages and look for UK postcodes in the content.
+
+Any incoming SMS messages are added to a simple listview. If a postcode is found, it appends ` - MAP AVAILABLE` to the message text.
+
+#### Screenshot
+<img src="https://github.com/jonnysmith1981/TestSMS/blob/master/Screenshot.png" alt="" width="45%" />
---
+++ app/src/main/java/com/example/jonathans/testsms/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.jonathans.testsms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> messages;
+    private ArrayAdapter<String> adapter;
+    private ListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        list = (ListView)findViewById(R.id.list_view);
+        messages = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.custom_listview, messages);
+
+        list.setAdapter(adapter);
+
+        registerReceiver(broadcastReceiver, new IntentFilter("NEW_SMS"));
+    }
+
+    BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle bundle = intent.getExtras();
+            String newMsg = (String)bundle.get("msg");
+            if (isPostCode(newMsg) == true) {
+                messages.add(newMsg + " - POSTCODE FOUND");
+            }
+            else {
+                messages.add(newMsg);
+            }
+            adapter.notifyDataSetChanged();
+        }
+    };
+
+    private boolean isPostCode(String str) {
+        Pattern p = Pattern.compile("(?:[A-Za-z]\\d ?\\d[A-Za-z]{2})|(?:[A-Za-z][A-Za-z\\d]\\d ?\\d[A-Za-z]{2})|(?:[A-Za-z]{2}\\d{2} ?\\d[A-Za-z]{2})|(?:[A-Za-z]\\d[A-Za-z] ?\\d[A-Za-z]{2})|(?:[A-Za-z]{2}\\d[A-Za-z] ?\\d[A-Za-z]{2})");
+        Matcher m = p.matcher(str);
+        if (m.find()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
